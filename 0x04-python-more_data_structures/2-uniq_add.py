1ebf15131c2cf885e805b411159f7595b63698ff
nassfatie <nassfatie@gmail.com>
2022-12-07 13:18:34 -0800

    unique addition

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-def uniq_add(my_lisst=[]):
-    if my_list is not None:
-        return sum(set(my_list))
-    return None
+def uniq_add(my_list=[]):
+    number = 0
+    for element in set(my_list):
+        number += element
+    return number
