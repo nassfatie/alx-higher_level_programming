cdcc229d4cb61c281825de5a8a1c63900b8d816b
nassfatie <nassfatie@gmail.com>
2022-12-08 13:53:28 -0800

    0-print_dlistint.c

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,11 +1,11 @@
 #include "lists.h"
 
 /**
- * print_dlistint - Prints all elements of a dlistint
- *
- * @h: head of the list
- * Return: Number of nodes
- */
+*print_dlistint - Prints all elements of a dlistint
+*
+*@h: head of the list
+*Return: Number of nodes
+*/
 size_t print_dlistint(const dlistint_t *h)
 {
 	int couter;
