599da2c9538a27a5ecf408cbcff7b43182f80467
nassfatie <nassfatie@gmail.com>
2023-02-14 14:52:12 -0800

    count

--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -1,2 +1,2 @@
--- script that inserts a new row in the table 
-INSERT INTO first_table(id, name) VALUES(98, 'Best School');
+-- Inserts a new row into the table first_table.
+INSERT INTO first_table(id, name) VALUES(89, 'Best School');
